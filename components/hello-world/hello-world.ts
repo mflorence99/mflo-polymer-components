06606cf38eebe34d65a580b368c3a92707136070
Mark Florence <mflo999@gmail.com>
2015-06-26 03:27:59 -0400

    Delete hello-world.ts

--- components/hello-world/hello-world.ts
+++
@@ -1,8 +0,0 @@
-class Student {
-  fullname : string;
-  constructor(public firstname, public middleinitial, public lastname) {
-    this.fullname = firstname + " " + middleinitial + " " + lastname;
-  }
-}
-
-var user = new Student("Jane", "M.", "User");
