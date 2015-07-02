b93dd570ce22f1479dc47f31038127c37b5a10e2
Mark Florence <mflo999@gmail.com>
2015-07-02 09:35:03 -0400

    Simplified styling
    No longer bother about not animating zero

--- components/mflo-spinning-numbers/mflo-spinning-numbers.js
+++ components/mflo-spinning-numbers/mflo-spinning-numbers.js
@@ -26,8 +26,8 @@ Polymer({
   valueChanged: function(value) {
     // out with the old
     Polymer.dom(this.$.container).innerHTML = "";
-    // in with the new
     if (typeof value !== "undefined") {
+      // in with the new
       var str = String(value).replace(/[\D]*/g, "");
       var num = Number(str);
       for (var i = 0; i < str.length; i++) {
@@ -37,11 +37,9 @@ Polymer({
         var offset = Number(str.charAt(i));
         // spin index delays visibility of number by position
         var spinx = Math.min(9, Math.max(0, (str.length - 1) - i));
-        var style = "top: calc(-" + offset + "em + 1px);";
-        if (num === 0)
-          style += " visibility: visible";
-        else Polymer.dom(inner).setAttribute("class", "spin spin" + spinx);
+        var style = "top: -" + offset + "em";
         Polymer.dom(inner).setAttribute("style", style);
+        Polymer.dom(inner).setAttribute("class", "spin spin" + spinx);
         Polymer.dom(inner).innerHTML = "0 1 2 3 4 5 6 7 8 9";
         Polymer.dom(outer).appendChild(inner);
         Polymer.dom(this.$.container).appendChild(outer);
